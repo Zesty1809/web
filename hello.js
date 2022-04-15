47b87c092486526148894fe5d877524b4250b367
Zesty1809 <103753480+Zesty1809@users.noreply.github.com>
2022-04-15 18:41:07 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,13 @@
+ function login()
+{
+	alert('Thank You for Login & You are Redirecting to Campuslife Website');
+  //Redirecting to other page or webste code or you can set your own html page.
+       window.location = "https://www.google.com";
+			}
+ function clearFunc()
+{
+		document.getElementById("name").value="";
+		document.getElementById("email").value="";
+		document.getElementById("username").value="";
+		document.getElementById("pwd").value="";
+	}
